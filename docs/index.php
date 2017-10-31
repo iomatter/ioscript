412cf648f741b86b0a1f66b7a22c66f43fab0ed2
iomatter <30539354+iomatter@users.noreply.github.com>
2017-10-30 18:44:32 -0700

    Create index.php

---
+++ docs/index.php
@@ -0,0 +1,6 @@
+<style>
+* {font-family: Arial;}
+body {background-color: #AFAFAF;}
+</style>
+<h1>This is the IOscript documentation <i>Currently Small</i></h1>
+<a href="syntax.html">IOscript Syntax Explanation</a>
